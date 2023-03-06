fd8cc5cd3fced80b4dc4d1dc16b5fb258c33eb02
Catalina <98126108+catalinatanaselea@users.noreply.github.com>
2023-03-06 19:54:57 +0200

    Add files via upload

---
+++ Nashvile Housing table.sql
@@ -0,0 +1,28 @@
+USE [MyDatabase]
+GO
+
+SELECT [UniqueID ]
+      ,[ParcelID]
+      ,[LandUse]
+      ,[SalePrice]
+      ,[LegalReference]
+      ,[SoldAsVacant]
+      ,[OwnerName]
+      ,[Acreage]
+      ,[LandValue]
+      ,[BuildingValue]
+      ,[TotalValue]
+      ,[YearBuilt]
+      ,[Bedrooms]
+      ,[FullBath]
+      ,[HalfBath]
+      ,[PropertySplitAddress]
+      ,[PropertySplitCity]
+      ,[OwnerSplitAddress]
+      ,[OwnerSplitCity]
+      ,[OwnerSplitState]
+  FROM [dbo].[NashvilleHousing]
+
+GO
+
+
